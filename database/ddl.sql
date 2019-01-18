ff75a5d61458bbb6df17c95ab6d83b3225da273b
softwarexyy <softwarexyy@126.com>
2019-01-18 18:37:25 +0800

    新建交易流水表的DDL

--- database/ddl.sql
+++ database/ddl.sql
@@ -27,3 +27,15 @@ CREATE TABLE `to_accinfo` (
   PRIMARY KEY (`userid`,`account`),
   UNIQUE KEY `account_UNIQUE` (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户账户信息表'
+
+CREATE TABLE `to_bill` (
+  `transFlowId` varchar(64) NOT NULL COMMENT '订单流水号',
+  `userid` varchar(64) DEFAULT NULL COMMENT '用户ID',
+  `account` varchar(32) DEFAULT NULL COMMENT '账号',
+  `date` datetime DEFAULT NULL COMMENT '交易时间',
+  `amount` decimal(18,2) DEFAULT NULL COMMENT '交易量',
+  `transType` varchar(64) DEFAULT NULL COMMENT '交易类型',
+  `transRemark` varchar(32) DEFAULT NULL COMMENT '交易备注',
+  PRIMARY KEY (`transFlowId`),
+  UNIQUE KEY `transFlowId_UNIQUE` (`transFlowId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='账单记录表'
